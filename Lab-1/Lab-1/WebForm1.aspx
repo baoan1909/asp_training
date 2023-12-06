86396f741ba528bb493ab38192cebf0697d5a2b0
baoan1909 <132035335+baoan1909@users.noreply.github.com>
2023-12-06 18:14:29 +0700

    Add files via upload

---
+++ Lab-1/Lab-1/WebForm1.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Lab_1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <div>
+                <asp:Label ID="Soa" runat="server" Text="Nhập số nguyên a"></asp:Label>
+                <asp:TextBox ID="tboSoa" runat="server" TextMode="Number"></asp:TextBox>
+            </div>
+            <br/>
+            <div>
+                <asp:Label ID="Sob" runat="server" Text="Nhập số nguyên b"></asp:Label>
+                <asp:TextBox ID="tboSob" runat="server" TextMode="Number"></asp:TextBox>
+            </div>
+            <br/>
+            <div>
+                <asp:Button ID="btnTinh" runat="server" Text="=" style="width:160px;" OnClick="btnTinh_Click" />
+            </div>
+            <br/>
+            <div>
+                <asp:Label ID="lblKetqua" runat="server" Text="kết quả"></asp:Label>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Lab-1/Lab-1/WebForm1.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lab_1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        protected void btnTinh_Click(object sender, EventArgs e)
+        {
+            TextBox tboSoa = (TextBox)FindControl("tboSoa");
+            TextBox tboSob = (TextBox)FindControl("tboSob");
+            
+            if(tboSoa != null && tboSob != null)
+            {
+                if(int.TryParse(tboSoa.Text, out int a) && int.TryParse(tboSob.Text, out int b))
+                {
+                   int sum = a + b;
+
+                    lblKetqua.Text = $"Kết quả: {sum}";
+                }
+                else
+                {
+                    lblKetqua.Text = "Vui lòng nhập số nguyên hợp lệ";
+                }
+            }
+            else
+            {
+                lblKetqua.Text = "Vui lòng nhập số nguyên vào ô dữ liệu";
+            }
+        }
+    }
+}
